46684f66d7dc5adbaaacb5c5e20dbc24a710776a
Jan Kaifer <jan@kaifer.cz>
2023-03-23 14:47:52 +0100

    First version of vercel/otel

---
+++ src/index.node.ts
@@ -0,0 +1,25 @@
+export * from '@opentelemetry/api';
+export * from '@opentelemetry/semantic-conventions';
+
+import { Resource } from '@opentelemetry/resources';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+import {
+  NodeTracerProvider,
+  SimpleSpanProcessor,
+} from '@opentelemetry/sdk-trace-node';
+
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+
+export const register = (serviceName: string) => {
+  const provider = new NodeTracerProvider({
+    resource: new Resource({
+      [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
+    }),
+  });
+
+  provider.register();
+
+  provider.addSpanProcessor(new SimpleSpanProcessor(new OTLPTraceExporter({})));
+
+  return provider;
+};
