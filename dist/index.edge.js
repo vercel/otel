b2489f3ed23c75a735a172116c8b6e75a7691fd4
Jan Kaifer <jan@kaifer.cz>
2023-03-28 15:28:13 +0200

    migrate to es module

--- dist/index.edge.js
+++ dist/index.edge.js
@@ -1,9 +1,5 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.registerOTel = void 0;
-const registerOTel = (serviceName) => {
+export const registerOTel = (serviceName) => {
     void serviceName;
     // We don't support OpenTelemetry on edge.
 };
-exports.registerOTel = registerOTel;
 //# sourceMappingURL=index.edge.js.map
--- dist/index.node.js
+++ dist/index.node.js
@@ -1,34 +1,16 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __exportStar = (this && this.__exportStar) || function(m, exports) {
-    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.registerOTel = void 0;
-__exportStar(require("@opentelemetry/api"), exports);
-const resources_1 = require("@opentelemetry/resources");
-const semantic_conventions_1 = require("@opentelemetry/semantic-conventions");
-const sdk_trace_node_1 = require("@opentelemetry/sdk-trace-node");
-const exporter_trace_otlp_http_1 = require("@opentelemetry/exporter-trace-otlp-http");
-const registerOTel = (serviceName) => {
-    const provider = new sdk_trace_node_1.NodeTracerProvider({
-        resource: new resources_1.Resource({
-            [semantic_conventions_1.SemanticResourceAttributes.SERVICE_NAME]: serviceName,
+export * from "@opentelemetry/api";
+import { Resource } from "@opentelemetry/resources";
+import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
+import { NodeTracerProvider, SimpleSpanProcessor, } from "@opentelemetry/sdk-trace-node";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+export const registerOTel = (serviceName) => {
+    const provider = new NodeTracerProvider({
+        resource: new Resource({
+            [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
         }),
     });
     provider.register();
-    provider.addSpanProcessor(new sdk_trace_node_1.SimpleSpanProcessor(new exporter_trace_otlp_http_1.OTLPTraceExporter({})));
+    provider.addSpanProcessor(new SimpleSpanProcessor(new OTLPTraceExporter({})));
     return provider;
 };
-exports.registerOTel = registerOTel;
 //# sourceMappingURL=index.node.js.map
