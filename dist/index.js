33869fc19829af3e8aa745e34fa57bb1a57af18a
Jan Kaifer <jan@kaifer.cz>
2023-03-29 13:06:27 +0200

    to commonjs

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,6 @@
-export const registerOTel = (serviceName) => serviceName;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.registerOTel = void 0;
+const registerOTel = (serviceName) => serviceName;
+exports.registerOTel = registerOTel;
 //# sourceMappingURL=index.js.map
