f0e360ff015dfe4adb8e13362b6a40919b954a8a
Jan Kaifer <jan@kaifer.cz>
2023-03-29 12:50:05 +0200

    emit just index

--- dist/register.node.d.ts
+++
@@ -1,4 +0,0 @@
-export * from "@opentelemetry/api";
-import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
-export declare const registerOTel: (serviceName: string) => NodeTracerProvider;
-//# sourceMappingURL=register.node.d.ts.map
