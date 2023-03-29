3a3581e0b1b17f1fc95909149f1aa0fd40be5569
Jan Kaifer <jan@kaifer.cz>
2023-03-29 10:34:42 +0200

    switch to grpc

---
+++ dist/index.edge.js
@@ -0,0 +1,5 @@
+export const registerOTel = (serviceName) => {
+    void serviceName;
+    // We don't support OpenTelemetry on edge.
+};
+//# sourceMappingURL=index.edge.js.map
--- dist/index.node.js
+++ dist/index.node.js
@@ -2,7 +2,7 @@ export * from "@opentelemetry/api";
 import { Resource } from "@opentelemetry/resources";
 import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
 import { ConsoleSpanExporter, NodeTracerProvider, SimpleSpanProcessor, } from "@opentelemetry/sdk-trace-node";
-import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-grpc";
 export const registerOTel = (serviceName) => {
     const provider = new NodeTracerProvider({
         resource: new Resource({
