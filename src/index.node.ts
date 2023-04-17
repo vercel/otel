97048bb42b85c38f14c18e3fab59665827237af2
Jan Kaifer <jan@kaifer.cz>
2023-04-17 16:11:02 +0200

    Migrate to node sdk (#4)

--- src/index.node.ts
+++ src/index.node.ts
@@ -1,22 +1,15 @@
+import { NodeSDK } from "@opentelemetry/sdk-node";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
 import { Resource } from "@opentelemetry/resources";
 import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
-import {
-  NodeTracerProvider,
-  SimpleSpanProcessor,
-} from "@opentelemetry/sdk-trace-node";
-
-import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-node";
 
 export const registerOTel = (serviceName: string) => {
-  const provider = new NodeTracerProvider({
+  const sdk = new NodeSDK({
     resource: new Resource({
       [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
     }),
+    spanProcessor: new SimpleSpanProcessor(new OTLPTraceExporter()),
   });
-
-  provider.register();
-
-  provider.addSpanProcessor(new SimpleSpanProcessor(new OTLPTraceExporter({})));
-
-  return provider;
+  sdk.start();
 };
