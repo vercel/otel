41616502326a65ff2e50db3bdcf898419d9ad439
Jan Kaifer <jan@kaifer.cz>
2023-04-27 18:30:04 +0200

    Switch to grpc and update dependencies (#6)

--- src/index.node.ts
+++ src/index.node.ts
@@ -1,5 +1,5 @@
 import { NodeSDK } from "@opentelemetry/sdk-node";
-import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-grpc";
 import { Resource } from "@opentelemetry/resources";
 import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
 import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-node";
