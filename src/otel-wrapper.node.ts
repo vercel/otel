1aee4d90c50d5ca96c8379c5be184fc997ff09bd
Stephen Firincieli <stephen.firrincieli@datadoghq.com>
2023-06-09 15:50:48 -0400

    Update docstrings

--- src/otel-wrapper.node.ts
+++ src/otel-wrapper.node.ts
@@ -10,18 +10,25 @@ import { setSpan } from "@opentelemetry/api/build/src/trace/context-utils";
 import { NextApiRequest, NextApiResponse } from "next/types";
 import { registerOTel } from "./register-otel.node";
 
-// Same as next/types/NextApiHandler with an optional context arg
-export type TracedNextApiHandler = (
+// Identical to next/types/NextApiHandler with an optional context arg
+export type TracedNextApiHandler<TReturn = unknown> = (
   req: NextApiRequest,
   res: NextApiResponse,
-  handlerSpan: Span,
-  oTelContext: Context
-) => unknown | Promise<unknown>;
+  spanAndContext: { handlerSpan: Span; oTelContext: Context }
+) => TReturn | Promise<TReturn>;
 
 const DEFAULT_OTEL_SERVICE_NAME = "next-app";
 
 const buildServiceTracerName = (serviceName: string) => `${serviceName}-tracer`;
 
+/**
+ * Create OTel context from traceparent and tracestate headers
+ *
+ * * See W3C spec: https://www.w3.org/TR/trace-context/#design-overview
+ *
+ * @param headers Next API route request headers
+ * @returns OTel active context
+ */
 const getActiveContext = (headers: NextApiRequest["headers"]) => {
   let traceParent = headers.traceparent;
 
@@ -43,11 +50,23 @@ const getActiveContext = (headers: NextApiRequest["headers"]) => {
 };
 
 let hasRegisteredOtel = false;
-
-export const traceWithOTel = (originalHandler: TracedNextApiHandler) => {
+/**
+ * Wrap Next API route handler to enable OTel tracing
+ *
+ * * Registers OTel service + tracer with name from env var OTEL_SERVICE_NAME
+ * * Starts a handler span
+ * * Catches errors, adds info to span, rethrows
+ *
+ * @param originalHandler Your original Next API Route handler
+ * @returns Updated handler with tracing enabled
+ */
+export const traceWithOTel = <TReturn>(
+  originalHandler: TracedNextApiHandler<TReturn>
+) => {
   const otelServiceName =
     process.env.OTEL_SERVICE_NAME ?? DEFAULT_OTEL_SERVICE_NAME;
 
+  // Register OTel if it hasn't been registered yet
   if (!hasRegisteredOtel) {
     registerOTel(otelServiceName);
     hasRegisteredOtel = true;
@@ -59,23 +78,18 @@ export const traceWithOTel = (originalHandler: TracedNextApiHandler) => {
     const activeContext = getActiveContext(req.headers);
 
     return await tracer.startActiveSpan(
-      "handler",
+      "next_api_route_handler",
       {},
       activeContext,
       async (handlerSpan) => {
-        const ctx = setSpan(context.active(), handlerSpan);
+        const oTelContext = setSpan(context.active(), handlerSpan);
 
-        // We can set extra attributes here on the span
-
-        // Catch exceptions and add their info to parent span
-        // before rethrowing
+        // Catch exceptions and add their info to parent span before rethrowing
         try {
-          const originalResponse = await originalHandler(
-            req,
-            res,
+          const originalResponse = await originalHandler(req, res, {
             handlerSpan,
-            ctx
-          );
+            oTelContext,
+          });
 
           handlerSpan.end();
 
