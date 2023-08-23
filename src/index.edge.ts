c0edc1338d56c37a02fd679abd64b854908e5b89
agadzik <andrew.gadzik@vercel.com>
2023-08-23 13:51:28 -0400

    update deps, add edge support

--- src/index.edge.ts
+++ src/index.edge.ts
@@ -1,4 +1,103 @@
+import "performance-polyfill";
+import {
+  BasicTracerProvider,
+  BatchSpanProcessor,
+  ReadableSpan,
+  SDKRegistrationConfig,
+  TracerConfig,
+} from "@opentelemetry/sdk-trace-base";
+import { AsyncLocalStorageContextManager } from "@opentelemetry/context-async-hooks";
+import { Resource } from "@opentelemetry/resources";
+import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
+import {
+  OTLPExporterBase,
+  OTLPExporterError,
+  OTLPExporterConfigBase,
+} from "@opentelemetry/otlp-exporter-base";
+import { diag } from "@opentelemetry/api";
+import { createExportTraceServiceRequest } from "@opentelemetry/otlp-transformer";
+import { IExportTraceServiceRequest } from "@opentelemetry/otlp-transformer";
+import { getEnv } from "@opentelemetry/core";
+
+const DEFAULT_COLLECTOR_RESOURCE_PATH = "v1/traces";
+const DEFAULT_COLLECTOR_URL = `http://localhost:4318/${DEFAULT_COLLECTOR_RESOURCE_PATH}`;
+
+class EdgeTraceProvider extends BasicTracerProvider {
+  constructor(config: TracerConfig = {}) {
+    super(config);
+  }
+
+  register(config: SDKRegistrationConfig = {}) {
+    if (config.contextManager === undefined) {
+      config.contextManager = new AsyncLocalStorageContextManager();
+      config.contextManager.enable();
+    }
+
+    super.register(config);
+  }
+}
+
+abstract class OTLPExporterEdgeBase<
+  ExportItem,
+  ServiceRequest
+> extends OTLPExporterBase<OTLPExporterConfigBase, ExportItem, ServiceRequest> {
+  onShutdown() {}
+  onInit(_config: OTLPExporterConfigBase) {}
+  send(
+    items: ExportItem[],
+    onSuccess: () => void,
+    onError: (error: OTLPExporterError) => void
+  ): void {
+    console.log("send");
+    if (this._shutdownOnce.isCalled) {
+      diag.debug("Shutdown already started. Cannot send objects");
+      return;
+    }
+    const serviceRequest = this.convert(items);
+
+    const promise = fetch(this.url, {
+      method: "POST",
+      body: JSON.stringify(serviceRequest),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // @ts-ignore
+      next: {
+        internal: true,
+      },
+    }).then(onSuccess, onError);
+
+    this._sendingPromises.push(promise);
+    const popPromise = () => {
+      const index = this._sendingPromises.indexOf(promise);
+      this._sendingPromises.splice(index, 1);
+    };
+    promise.then(popPromise, popPromise);
+  }
+}
+
+class OTLPTraceExporter extends OTLPExporterEdgeBase<
+  ReadableSpan,
+  IExportTraceServiceRequest
+> {
+  convert(spans: ReadableSpan[]): IExportTraceServiceRequest {
+    return createExportTraceServiceRequest(spans, true);
+  }
+  getDefaultUrl(config: OTLPExporterConfigBase) {
+    return typeof config.url === "string"
+      ? config.url
+      : getEnv().OTEL_EXPORTER_OTLP_TRACES_ENDPOINT.length > 0
+      ? getEnv().OTEL_EXPORTER_OTLP_TRACES_ENDPOINT
+      : DEFAULT_COLLECTOR_URL;
+  }
+}
+
 export const registerOTel = (serviceName: string) => {
-  // We don't support OTel on edge yet
-  void serviceName;
+  const provider = new EdgeTraceProvider({
+    resource: new Resource({
+      [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
+    }),
+  });
+  provider.addSpanProcessor(new BatchSpanProcessor(new OTLPTraceExporter()));
+  provider.register();
 };
--- src/index.node.ts
+++ src/index.node.ts
@@ -1,15 +1,15 @@
 import { NodeSDK } from "@opentelemetry/sdk-node";
-import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-grpc";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
 import { Resource } from "@opentelemetry/resources";
 import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
-import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-node";
+import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-node";
 
 export const registerOTel = (serviceName: string) => {
   const sdk = new NodeSDK({
     resource: new Resource({
       [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
     }),
-    spanProcessor: new SimpleSpanProcessor(new OTLPTraceExporter()),
+    spanProcessor: new BatchSpanProcessor(new OTLPTraceExporter()),
   });
   sdk.start();
 };
