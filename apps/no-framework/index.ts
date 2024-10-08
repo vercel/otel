b944f6a016508dad55da1684e6918fccc676e112
dvoytenko <dima.voytenko@vercel.com>
2024-10-08 10:44:51 -0700

    docs

--- apps/no-framework/index.ts
+++ apps/no-framework/index.ts
@@ -1,4 +1,7 @@
+// This is the only import for user code.
 import { trace as tracing } from "@opentelemetry/api";
+
+// All these imports are for SDK configuration, not user code.
 import {
   AlwaysOnSampler,
   BasicTracerProvider,
@@ -10,7 +13,10 @@ import { AsyncLocalStorageContextManager } from "@opentelemetry/context-async-ho
 import { Resource } from "@opentelemetry/resources";
 import { createExportTraceServiceRequest } from "@opentelemetry/otlp-transformer";
 
+// This is SDK code. A normal user code doesn't write it, but it calls some
+// SDK configuration, such as `registerOTel` from `@vercel/otel`.
 function setupTelemetry() {
+  // OTEL calls this "resource", while others (such as DataDog) call it "service".
   const resource = new Resource({
     "service.name": "my-app",
     "node.env": process.env.NODE_ENV,
@@ -22,8 +28,13 @@ function setupTelemetry() {
 
   const spanExporter: SpanExporter = {
     export: (spans, resultCallback) => {
+      // Converts a Span to IResourceSpans > IScopeSpans > ISpan structure, which
+      // is OTLP format. It's can be directly serialized to JSON or converted
+      // to Protobuf.
       const resourceSpans = createExportTraceServiceRequest(spans, {
+        // Uses hex-encoding trace and span IDs. Otherwise, base64 is used.
         useHex: true,
+        // Uses `{high, low}` format for timestamps. Otherwise, `unixNanon` is used.
         useLongBits: true,
       });
       console.log("[TEL]", JSON.stringify(resourceSpans));
@@ -39,10 +50,17 @@ function setupTelemetry() {
     idGenerator,
     sampler: new AlwaysOnSampler(),
   });
+
+  // Simplification. A SimpleSpanProcessor would almost never be used in a real
+  // case for exporting.
   tracerProvider.addSpanProcessor(new SimpleSpanProcessor(spanExporter));
+
   tracerProvider.register({ contextManager });
 }
 
+setupTelemetry();
+
+// User code. It only uses `@opentelemetry/api` APIs.
 async function handler() {
   const tracer = tracing.getTracer("app");
   return tracer.startActiveSpan(
@@ -60,7 +78,6 @@ async function handler() {
   );
 }
 
-setupTelemetry();
 void handler().then((result) => {
   console.log("Complete. Result = [", result, "]");
 });
