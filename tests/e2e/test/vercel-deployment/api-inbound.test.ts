b48c98b6228855ad64994c1b188580f248114d27
Darpan Kakadia <kakadiadarpan@gmail.com>
2025-06-02 18:13:03 +0200

    add http inbound tests

--- tests/e2e/test/vercel-deployment/api-inbound.test.ts
+++ tests/e2e/test/vercel-deployment/api-inbound.test.ts
@@ -3,7 +3,7 @@ import { SpanKind, SpanStatusCode } from "@opentelemetry/api";
 import { describe } from "../../lib/with-bridge";
 import { expectTrace } from "../../lib/expect-trace";
 
-describe("vercel deployment: api inbound", {}, (props) => {
+describe("vercel deployment: api inbound with fetch", {}, (props) => {
   it("should trace api for serverless", async () => {
     const { port, collector, bridge } = props();
 
@@ -52,7 +52,7 @@ describe("vercel deployment: api inbound", {}, (props) => {
             {
               name: "sample-span",
               kind: SpanKind.INTERNAL,
-              attributes: { scope: "sample", foo: "bar" },
+              attributes: { scope: "sample", foo: "bar", "fetch.type": "fetch" },
               spans: [
                 {
                   name: `fetch POST http://localhost:${bridge.port}/`,
@@ -121,7 +121,7 @@ describe("vercel deployment: api inbound", {}, (props) => {
             {
               name: "sample-span",
               kind: SpanKind.INTERNAL,
-              attributes: { scope: "sample", foo: "bar" },
+              attributes: { scope: "sample", foo: "bar", "fetch.type": "fetch" },
               spans: [
                 {
                   name: `fetch POST http://localhost:${bridge.port}/`,
@@ -174,7 +174,7 @@ describe("vercel deployment: api inbound", {}, (props) => {
         {
           name: "sample-span",
           kind: SpanKind.INTERNAL,
-          attributes: { scope: "sample", foo: "bar" },
+          attributes: { scope: "sample", foo: "bar", "fetch.type": "fetch" },
           spans: [
             {
               name: `fetch POST http://localhost:${bridge.port}/`,
@@ -186,6 +186,146 @@ describe("vercel deployment: api inbound", {}, (props) => {
                 "net.peer.port": `${bridge.port}`,
               },
               spans: [],
+            },
+          ],
+        },
+      ],
+    });
+  });
+});
+
+describe("vercel deployment: api inbound with http", {}, (props) => {
+  it("should trace api for serverless", async () => {
+    const { port, collector, bridge } = props();
+
+    const execResp = await bridge.fetch(
+      `/api/service/baz?dataUrl=${encodeURIComponent(
+        `http://localhost:${bridge.port}`
+      )}&fetchType=http`
+    );
+    expect(execResp.status).toBe(200);
+    expect(await execResp.text()).toEqual("Success baz bar");
+
+    await expectTrace(collector, {
+      name: "GET /api/service/[slug]/route",
+      status: { code: SpanStatusCode.UNSET },
+      kind: SpanKind.SERVER,
+      parentSpanId: undefined,
+      resource: {
+        "service.name": "sample-app",
+        "vercel.runtime": "nodejs",
+        env: "test",
+      },
+      attributes: {
+        scope: "next.js",
+        client: "bridge",
+        "vercel.request_id": "request1",
+        "next.span_name": "GET /api/service/[slug]/route",
+        "next.span_type": "BaseServer.handleRequest",
+        "http.method": "GET",
+        "http.host": `127.0.0.1:${port}`,
+        "http.status_code": 200,
+        "next.route": "/api/service/[slug]/route",
+        "http.route": "/api/service/[slug]/route",
+      },
+      spans: [
+        {
+          name: "executing api route (app) /api/service/[slug]/route",
+          kind: SpanKind.INTERNAL,
+          attributes: {
+            scope: "next.js",
+            "next.span_name":
+              "executing api route (app) /api/service/[slug]/route",
+            "next.span_type": "AppRouteRouteHandlers.runHandler",
+            "next.route": "/api/service/[slug]/route",
+          },
+          spans: [
+            {
+              name: "sample-span",
+              kind: SpanKind.INTERNAL,
+              attributes: { scope: "sample", foo: "bar", "fetch.type": "http" },
+              spans: [
+                {
+                  name: `http POST http://localhost:${bridge.port}/`,
+                  attributes: {
+                    scope: "@vercel/otel/fetch",
+                    "http.method": "POST",
+                    "http.url": `http://localhost:${bridge.port}/`,
+                    "net.peer.name": "localhost",
+                    "net.peer.port": `${bridge.port}`,
+                  },
+                  spans: [],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    });
+  });
+
+  it.only("should trace api with FormData for serverless", async () => {
+    const { port, collector, bridge } = props();
+
+    const execResp = await bridge.fetch(
+      `/api/service/baz?dataUrl=${encodeURIComponent(
+        `http://localhost:${bridge.port}`
+      )}&mode=formdata&fetchType=http`
+    );
+    expect(execResp.status).toBe(200);
+    expect(await execResp.text()).toEqual("Success baz bar");
+
+    await expectTrace(collector, {
+      name: "GET /api/service/[slug]/route",
+      status: { code: SpanStatusCode.UNSET },
+      kind: SpanKind.SERVER,
+      parentSpanId: undefined,
+      resource: {
+        "service.name": "sample-app",
+        "vercel.runtime": "nodejs",
+        env: "test",
+      },
+      attributes: {
+        scope: "next.js",
+        client: "bridge",
+        "vercel.request_id": "request1",
+        "next.span_name": "GET /api/service/[slug]/route",
+        "next.span_type": "BaseServer.handleRequest",
+        "http.method": "GET",
+        "http.host": `127.0.0.1:${port}`,
+        "http.status_code": 200,
+        "next.route": "/api/service/[slug]/route",
+        "http.route": "/api/service/[slug]/route",
+      },
+      spans: [
+        {
+          name: "executing api route (app) /api/service/[slug]/route",
+          kind: SpanKind.INTERNAL,
+          attributes: {
+            scope: "next.js",
+            "next.span_name":
+              "executing api route (app) /api/service/[slug]/route",
+            "next.span_type": "AppRouteRouteHandlers.runHandler",
+            "next.route": "/api/service/[slug]/route",
+          },
+          spans: [
+            {
+              name: "sample-span",
+              kind: SpanKind.INTERNAL,
+              attributes: { scope: "sample", foo: "bar", "fetch.type": "http" },
+              spans: [
+                {
+                  name: `http POST http://localhost:${bridge.port}/`,
+                  attributes: {
+                    scope: "@vercel/otel/fetch",
+                    "http.method": "POST",
+                    "http.url": `http://localhost:${bridge.port}/`,
+                    "net.peer.name": "localhost",
+                    "net.peer.port": `${bridge.port}`,
+                  },
+                  spans: [],
+                },
+              ],
             },
           ],
         },
