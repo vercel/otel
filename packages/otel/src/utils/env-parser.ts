d62a92d1505b0ba3d13ad442bcaba777275e8417
Darpan Kakadia <kakadiadarpan@gmail.com>
2025-09-25 12:23:55 +0200

    [env-parser] eslint and add comment

--- packages/otel/src/utils/env-parser.ts
+++ packages/otel/src/utils/env-parser.ts
@@ -1,8 +1,13 @@
+/**
+ * This functions have been copied from the `@opentelemetry/core` package
+ * as using it directly causes issues with the Edge runtime.
+*/
+
 /** @internal */
 export function getStringFromEnv(key: string): string | undefined {
   const raw = process.env[key];
-  if (raw == null || raw.trim() === "") {
-    return undefined;
+  if (!raw || raw.trim() === "") {
+      return undefined;
   }
   return raw;
 }
@@ -11,6 +16,6 @@ export function getStringFromEnv(key: string): string | undefined {
 export function getStringListFromEnv(key: string): string[] | undefined {
   return getStringFromEnv(key)
     ?.split(",")
-    .map((v) => v.trim())
-    .filter((s) => s !== "");
+    .map(v => v.trim())
+    .filter(s => s !== "");
 }
