5c4eba48acf8e6e707af43802ea80b8886a0c711
Darpan Kakadia <kakadiadarpan@gmail.com>
2026-04-07 14:36:38 +0200

    add new attributes

--- packages/otel/src/semantic-resource-attributes.ts
+++ packages/otel/src/semantic-resource-attributes.ts
@@ -9,10 +9,19 @@ export const HTTP_RESPONSE_CONTENT_LENGTH_UNCOMPRESSED =
 export const NET_PEER_PORT = "net.peer.port";
 export const NET_PEER_NAME = "net.peer.name";
 export const CLOUD_REGION = "cloud.region";
+/** @deprecated Use DEPLOYMENT_ENVIRONMENT_NAME instead. */
 export const DEPLOYMENT_ENVIRONMENT = "deployment.environment";
+export const DEPLOYMENT_ENVIRONMENT_NAME = "deployment.environment.name";
+export const DEPLOYMENT_ID = "deployment.id";
 export const CLOUD_PROVIDER = "cloud.provider";
 export const PROCESS_RUNTIME_NAME = "process.runtime.name";
-export const VCS_REPOSITORY_REF_VERSION = "vcs.repository.ref.revision";
+export const VCS_REF_HEAD_NAME = "vcs.ref.head.name";
+export const VCS_REF_HEAD_REVISION = "vcs.ref.head.revision";
+export const VCS_REF_TYPE = "vcs.ref.type";
+export const VCS_REPOSITORY_NAME = "vcs.repository.name";
+export const VCS_REPOSITORY_REF_REVISION = "vcs.repository.ref.revision";
+/** @deprecated Use VCS_REPOSITORY_REF_REVISION instead. */
+export const VCS_REPOSITORY_REF_VERSION = VCS_REPOSITORY_REF_REVISION;
 export const FAAS_INVOKED_REGION = "faas.invoked_region";
 export const FAAS_INVOCATION_ID = "faas.invocation_id";
 export const SERVICE_NAME = "service.name";
